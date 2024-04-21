833c4b39c1404cb9dfaa5a4edbda19b6a877df07
Olivier Bouillet <freeboub@gmail.com>
2024-04-21 14:31:42 +0200

    fix: clean up sample

--- App.tsx
+++ App.tsx
@@ -5,11 +5,11 @@
  * @format
  */
 
-import {Text, View, Pressable, Alert} from 'react-native';
-import React, {PropsWithChildren, useEffect, useMemo, useState} from 'react';
-import styles from './styles';
-import {complexArray, complexDuration, complexTimeout} from './constants';
+import React, {useEffect, useState} from 'react';
 
+export const timeout = 10;
+
+/* component which is too long to render */
 const HeavyComponent = () => {
   let i = 0;
   while (i < 300000) {
@@ -18,92 +18,19 @@ const HeavyComponent = () => {
   return null;
 };
 
-/* define the button */
-type MyCounterProps = PropsWithChildren<{
-  index: number;
-  time: number;
-}>;
-
-const MyCounter = React.memo(({index, time}: MyCounterProps) => {
-  const isInFirstQuartil = time <= complexDuration / 4;
-  const isInSecondQuartil =
-    time > complexDuration / 4 && time <= complexDuration / 2;
-
-  const isInThirdQuartil =
-    time > complexDuration / 2 && time <= (complexDuration * 3) / 4;
-
-  const isInLastQuartil =
-    !isInFirstQuartil && !isInSecondQuartil && !isInThirdQuartil;
-
-  const onPressFunction = () => {
-    Alert.alert(
-      'Alert',
-      'press at ' +
-        time +
-        ' \nfirstQuartil ' +
-        isInFirstQuartil +
-        ' \nsecondQuartil ' +
-        isInSecondQuartil +
-        ' \nThirdQuartil ' +
-        isInThirdQuartil +
-        ' \nLastQuartil ' +
-        isInLastQuartil,
-    );
-  };
-
-  const isInQuartil =
-    index === 0
-      ? isInFirstQuartil
-      : index === 1
-      ? isInSecondQuartil
-      : index === 2
-      ? isInThirdQuartil
-      : isInLastQuartil;
-
-  const backgroundColor = isInQuartil ? 'red' : 'green';
-  const viewStyle = {backgroundColor, height: 100, width: 100};
-
-  return (
-    <Pressable onPress={onPressFunction}>
-      <View style={viewStyle}>
-        <HeavyComponent />
-      </View>
-    </Pressable>
-  );
-});
-
 /* core of the App */
 
 const App = () => {
-  const [time, setTime] = useState(0);
+  // setState on timeout to for refresh
+  const [_, setTime] = useState(0);
 
   useEffect(() => {
     setInterval(() => {
-      setTime(_time => (_time + 1) % complexDuration);
-    }, complexTimeout);
+      setTime(_time => _time + 1);
+    }, timeout);
   }, []);
 
-  const myButtonList = useMemo(() => {
-    return (
-      <>
-        {complexArray.map(id => {
-          return <MyCounter key={id} index={id} time={time} />;
-        })}
-      </>
-    );
-  }, [time]);
-
-  const result = (
-    <View style={styles.container}>
-      <Text>Time: {time}</Text>
-      {myButtonList}
-    </View>
-  );
-  return result;
+  return <HeavyComponent />;
 };
 
-const ContextedApp = () => {
-  return <App />;
-};
-
-export default ContextedApp;
+export default App;
--- constants.ts
+++
@@ -1,7 +0,0 @@
-export const nbButton = 300;
-export const array = Array.from(Array(nbButton).keys());
-
-export const complexNbButton = 4;
-export const complexArray = Array.from(Array(complexNbButton).keys());
-export const complexDuration = 20;
-export const complexTimeout = 50;
